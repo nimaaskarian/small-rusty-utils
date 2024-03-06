d0ae6a0332b56b80cd70e77703166af5ebcabd95
nimaaskarian <nimaaskarian.work@outlook.com>
2024-03-06 18:33:11 +0330

    Added gitignore to ignore everything besides .rs Added reverse to qwerty fix fa

--- qwerty_fix_fa.rs
+++ qwerty_fix_fa.rs
@@ -1,14 +1,25 @@
 use std::io::{self, BufRead};
+use std::env;
 
-fn farsi_to_english(input: &str) -> String {
-    let farsi_keys: Vec<u16> = "ضصثقفغعهخحجچشسیبلاتنمکگظطزرذدپو./۱۲۳۴۵۶۷۸۹۰".encode_utf16().collect();
-    let english_keys: Vec<u16> = "qwertyuiop[]asdfghjkl;'zxcvbnm,./1234567890".encode_utf16().collect();
+const EN_KEYS: &str = "qwertyuiop[]asdfghjkl;'zxcvbnm,./1234567890";
+const FA_KEYS: &str = "ضصثقفغعهخحجچشسیبلاتنمکگظطزرذدپو./۱۲۳۴۵۶۷۸۹۰";
+fn farsi_to_english(input: &str, reverse: bool) -> String {
+    let src: Vec<u16> = if reverse {
+        EN_KEYS.encode_utf16().collect()
+    } else {
+        FA_KEYS.encode_utf16().collect()
+    };
+    let dst: Vec<u16> = if reverse {
+        FA_KEYS.encode_utf16().collect()
+    } else {
+        EN_KEYS.encode_utf16().collect()
+    };
 
     let mut result: Vec<u16> = vec![];
 
     for c in input.encode_utf16() {
-        if let Some(index) = farsi_keys.iter().position(|&r| r == c) {
-            result.push(english_keys[index]);
+        if let Some(index) = src.iter().position(|&r| r == c) {
+            result.push(dst[index]);
         } else {
             result.push(c);
         }
@@ -18,11 +29,20 @@ fn farsi_to_english(input: &str) -> String {
 }
 
 fn main() -> std::io::Result<()>{
+
+    let reverse = {
+        let mut reverse = false;
+        let args: Vec<String> = env::args().collect();
+        if args.len() > 1 && args[1] == String::from("-r") {
+            reverse = true
+        }
+        reverse
+    };
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
 
     while let Some(line) = lines.next(){
-        println!("{}", farsi_to_english(&line?));
+        println!("{}", farsi_to_english(&line?, reverse));
     }
     Ok(())
 }
